6489ad3a1ae92744f17d2ce98dcd6b7c63fa07f7
Richard <richlloydmiles@gmail.com>
2023-07-30 21:07:00 +0200

    initial commit

---
+++ hello-world.test.ts
@@ -0,0 +1,12 @@
+import { expect } from '@jest/globals'
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('buildEmail', () => {
+  test('buildEmail builds email as expected', async () => {
+    expect(true).toBe(true)
+  })
+})
+
